3e5b17cef454206f190a12e2ff0e4f293383a5b9
kenan <xinkenan.11@hotmail.com>
2023-06-05 01:04:26 +0800

    added exercise 3.4 and 3.5 solution

---
+++ exercise3.4_substitute_cipher/src/exercise3.4_substitute_cipher.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : 4_substitute_cipher.cpp
+// Author      : kenan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	const char cipherChars[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'N', 'O', 'P', 'Q', };
+
+	char inputChar = INFINITY;
+
+	cout << "Enter PLAIN TEXT consist of only upper case characters , and ."
+			<< endl;
+
+	while ((inputChar = cin.get()) != 10) {
+		int cipherCharPotition = inputChar - 'A';
+		char cipherChar = cipherChars[cipherCharPotition];
+		if (inputChar == ',' || inputChar == '.' || inputChar == ' ') {
+			cout << inputChar;
+		} else {
+			cout << cipherChar;
+		}
+	}
+
+}
---
+++ exercise3.5_cipher_to_plain/src/exercise3.5_cipher_to_plain.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : 5_cipher_to_plain.cpp
+// Author      : kenan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int findCharPosition(const char[], int, char);
+
+int main() {
+
+	char inputChar = INFINITY;
+
+	const char cipherChars[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'N', 'O', 'P', 'Q', };
+
+	cout << "Enter CIPHER TEXT:" << endl;
+
+	while ((inputChar = cin.get()) != 10) {
+
+		if (inputChar == ',' || inputChar == '.' || inputChar == ' ') {
+			cout << inputChar;
+		} else {
+			const int pos = findCharPosition(cipherChars, 26, inputChar);
+			const char plainTextChar = 'A' + pos;
+			cout << plainTextChar;
+		}
+
+	}
+
+	return 0;
+}
+
+int findCharPosition(const char charArray[], int size, char character) {
+	for (int i = 0; i < size; i++) {
+		if (charArray[i] == character) {
+			return i;
+		}
+	}
+	return -1;
+}
